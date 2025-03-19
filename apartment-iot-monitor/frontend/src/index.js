abd03c8e4002427e25aa7d31fc42e07765f64780
timurboys112 <120889499+timurboys112@users.noreply.github.com>
2025-03-19 14:58:06 +0700

    Add apartment-iot-monitor/frontend/src/index.js 1742371082189

---
+++ apartment-iot-monitor/frontend/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles.css';
+import { Toaster } from 'react-hot-toast';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: '#333',
+            color: '#fff',
+          },
+        }}
+      />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
